b3d54ea124d33748034bab39a8ae46afb1d9b0e3
kaelif <flanagan.kaeli@gmail.com>
2020-05-24 17:29:11 -0700

    added scalability

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
+
+const responsive = {
+    0: {
+        items: 1
+    },
+    320: {
+        items: 1
+    },
+    560: {
+        items: 2
+    },
+    960: {
+        items: 3
+    }
+}
 $(document).ready(function(){
 
     $nav = $(".nav");
@@ -16,6 +31,7 @@ $(document).ready(function(){
         dots: false,
         nav: true,
         navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')]
+        responsive: responsive
     });
 
     // Click to scoll to top
